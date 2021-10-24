427f02df8687b72a96826b487b142e54a36c21e0
StefanKantardzhiev <stefan.ivailov@gmail.com>
2021-10-24 13:05:12 +0200

    adding materials

---
+++ While Loop/While Loop Lab/sequence2k1.js
@@ -0,0 +1,27 @@
+function test(input){
+
+    let n = Number(input[0]);
+    let k = 1;
+    
+    while (k <= n){
+        console.log(k)
+        k = k*2 + 1
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+test([3])
